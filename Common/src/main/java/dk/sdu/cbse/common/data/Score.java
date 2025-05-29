717835825a0260104f88dfac689dd02d0984c8c9
Magnus Thomsen <magnus@thomsen.one>
2025-05-29 22:14:39 +0200

    Added exceptionhandling for score

--- Common/src/main/java/dk/sdu/cbse/common/data/Score.java
+++ Common/src/main/java/dk/sdu/cbse/common/data/Score.java
@@ -1,31 +1,50 @@
 package dk.sdu.cbse.common.data;
 
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public final class Score {
 
     /**
-     * RestTemplate for adding score using http requests.
+     * RestTemplate for adding score using HTTP requests.
      */
-    RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
 
     /**
-     * Add score using .
+     * Adds score by sending a request to the score service.
+     *
      * @param amount Score amount to add.
      */
     public void add(final int amount) {
-        restTemplate.getForObject("http://localhost:8080/score?points=" + amount, String.class);
+        try {
+            restTemplate.getForObject("http://localhost:8080/score?points=" + amount, String.class);
+        } catch (RestClientException e) {
+            System.err.println("Failed to add score: " + e.getMessage());
+        }
     }
 
+    /**
+     * Retrieves the current score.
+     *
+     * @return the current score as a string, or null if the request fails.
+     */
     public String getScore() {
-        return restTemplate.getForObject("http://localhost:8080/current", String.class);
+        try {
+            return restTemplate.getForObject("http://localhost:8080/current", String.class);
+        } catch (RestClientException e) {
+            System.err.println("Failed to get score: " + e.getMessage());
+            return null;
+        }
     }
 
     /**
      * Resets the score.
      */
     public void reset() {
-        restTemplate.getForObject("http://localhost:8080/reset", String.class);
+        try {
+            restTemplate.getForObject("http://localhost:8080/reset", String.class);
+        } catch (RestClientException e) {
+            System.err.println("Failed to reset score: " + e.getMessage());
+        }
     }
-
 }
